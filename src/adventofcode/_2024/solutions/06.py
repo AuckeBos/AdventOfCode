a04cc7b6ec64debf3156d47aa85b8c374e0b81fa
Aucke Bos <aucke.bos@valid.nl>
2024-12-08 17:58:08 +0100

    feat: 2024-06-2 wip

--- src/adventofcode/_2024/solutions/06.py
+++ src/adventofcode/_2024/solutions/06.py
@@ -1,6 +1,8 @@
 from copy import deepcopy
 
 import numpy as np
+from pqdm.processes import pqdm
+from tqdm import tqdm
 
 from adventofcode._templates.v20231204.puzzle_to_solve import PuzzleToSolve
 from adventofcode.helpers.base_matrix import BaseMatrix, Direction, Directions, Position
@@ -10,6 +12,13 @@ class Map(BaseMatrix):
     obstructed_position: Position | None = None
     guard_history: list[tuple[Position, Direction]] | None = None
     mask: np.matrix | None = None
+
+    DIRECTION_TO_CHAR = {
+        Directions.TOP.value: "^",
+        Directions.RIGHT.value: ">",
+        Directions.BOTTOM.value: "v",
+        Directions.LEFT.value: "<",
+    }
 
     def is_blocked(self, position: Position) -> bool:
         return self[position] == "#" if self.is_in_bounds(position) else False
@@ -21,6 +30,10 @@ class Map(BaseMatrix):
     def obstruct(self, position: Position) -> None:
         self[position] = "#"
         self.obstructed_position = position
+
+    def destruct(self, position: Position) -> None:
+        self[position] = "."
+        self.obstructed_position = None
 
     def get_result_by_history(
         self,
@@ -57,23 +70,37 @@ class Map(BaseMatrix):
         self,
         position: Position = None,
         direction: Direction = None,
+        guard_history: list[tuple[Position, Direction]] = None,
     ) -> int | None:
         position = position or Position(*np.argwhere(self.data == "^")[0])
         direction = direction or Directions.TOP.value
         self.mask = self.data.copy()
-        self.guard_history = []
-        self.mask[position.tuple_] = "X"
+        if self.is_obstructed:
+            self.mask[*self.obstructed_position.tuple_] = "O"
+        self.guard_history = guard_history or []
         while self.is_in_bounds(position):
             if (position, direction) in self.guard_history:
-                print("Breaking loop")
+                self.mask[*position.tuple_] = "!"
+                # print(self.obstructed_position)
+                # print(self.matrix_to_str(self.mask) + "\n\n")
                 return None
-            self.mask[*position.tuple_] = "X"
+            self.guard_history.append((position, direction))
+            self.mask[*position.tuple_] = self.DIRECTION_TO_CHAR[direction]
             if self.is_blocked(position + direction):
                 direction = direction.turn_right()
-            self.guard_history.append((position, direction))
             position += direction
-        print("Done")
-        return np.count_nonzero(self.mask == "X")
+        return np.count_nonzero(np.isin(self.mask, ["^", ">", "v", "<", "!", "O"]))
+
+
+def try_one(map: Map, i: int) -> int:
+    position_to_block, _ = map.guard_history[i]
+    start_position, start_direction = map.guard_history[i - 1]
+    # start_direction = start_direction.turn_right()
+    map.obstruct(position_to_block)
+    result = map.walk_until_exit(
+        start_position, start_direction, [*map.guard_history[: i - 1]]
+    )
+    return 1 if result is None else 0
 
 
 class Puzzle6(PuzzleToSolve):
@@ -117,38 +144,46 @@ class Puzzle6(PuzzleToSolve):
         map_count = 0
         original_map = deepcopy(start_map)
         original_map.walk_until_exit()
-        original_mask = original_map.mask
-        for position in start_map.iter_topleft_to_bottomright():
-            # start, or obstruction, or never been in original walk
-            if (
-                start_map[position] in ["#", "^"]
-                or original_mask[position.tuple_] != "X"
-            ):
+
+        # for i in tqdm(range(1, len(original_map.guard_history))):
+        # start = 4125 if len(original_map.guard_history) > 4120 else 1
+        start = 1
+        args = []
+        tried = []
+        for i in range(start, len(original_map.guard_history)):
+            position = original_map.guard_history[i][0]
+            if position in tried:
                 continue
-            if position == Position(1, 4):
+            tried.append(position)
+            args.append((original_map, i))
+
+        result = pqdm(args, try_one, n_jobs=15, argument_type="args")
+
+        return sum(result)
+
+        for i in tqdm(range(start, len(original_map.guard_history))):
+            # for i in range(start, len(original_map.guard_history)):
+            # print(
+            #     f"{i}/{len(original_map.guard_history)} ({i/len(original_map.guard_history)*100:.2f}%)",
+            #     end="\r",
+            # )
+            position_to_block, _ = original_map.guard_history[i]
+            start_position, start_direction = original_map.guard_history[i - 1]
+            start_direction = start_direction.turn_right()
+            if start_position == Position(1, 4):
                 test = ""
-            # get the moment in time where we were at this position
-            moments_at_position = [
-                (i, (p, d))
-                for i, (p, d) in enumerate(original_map.guard_history)
-                if p == position
-            ]
-            start_position, start_direction = original_map.guard_history[
-                [
-                    i
-                    for (i, (p, d)) in enumerate(original_map.guard_history)
-                    if p == position
-                ][0]
-                - 1
-            ]
-            map = deepcopy(start_map)
-            map.obstruct(position)
-            result = map.walk_until_exit(start_position, start_direction)
+            # map = deepcopy(start_map)
+            start_map.obstruct(position_to_block)
+            result = start_map.walk_until_exit(
+                start_position, start_direction, [*original_map.guard_history[: i - 1]]
+            )
             if result is None:
                 map_count += 1
+            start_map.destruct(position_to_block)
 
         return map_count
 
 
-puzzle = Puzzle6()
-puzzle.solve()
+if __name__ == "__main__":
+    puzzle = Puzzle6()
+    puzzle.solve()
