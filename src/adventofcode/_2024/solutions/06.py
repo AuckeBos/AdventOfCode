01d112bd272db93d16f9a01de4e3f861f973452d
Aucke Bos <aucke.bos@valid.nl>
2024-12-10 19:19:44 +0100

    feat: 2024-06-02

--- src/adventofcode/_2024/solutions/06.py
+++ src/adventofcode/_2024/solutions/06.py
@@ -1,106 +1,44 @@
-from copy import deepcopy
+from collections import defaultdict
 
 import numpy as np
-from pqdm.processes import pqdm
 from tqdm import tqdm
 
 from adventofcode._templates.v20231204.puzzle_to_solve import PuzzleToSolve
 from adventofcode.helpers.base_matrix import BaseMatrix, Direction, Directions, Position
 
 
-class Map(BaseMatrix):
-    obstructed_position: Position | None = None
-    guard_history: list[tuple[Position, Direction]] | None = None
-    mask: np.matrix | None = None
+class InfiniteLoopError(Exception):
+    def __init__(self):
+        super().__init__("Guard stuck in loop")
 
-    DIRECTION_TO_CHAR = {
-        Directions.TOP.value: "^",
-        Directions.RIGHT.value: ">",
-        Directions.BOTTOM.value: "v",
-        Directions.LEFT.value: "<",
-    }
+
+class Map(BaseMatrix):
+    def __init__(self, input_: str):
+        super().__init__(input_, None, str)
 
     def is_blocked(self, position: Position) -> bool:
         return self[position] == "#" if self.is_in_bounds(position) else False
 
-    @property
-    def is_obstructed(self) -> bool:
-        return self.obstructed_position is not None
-
-    def obstruct(self, position: Position) -> None:
-        self[position] = "#"
-        self.obstructed_position = position
-
-    def destruct(self, position: Position) -> None:
-        self[position] = "."
-        self.obstructed_position = None
-
-    def get_result_by_history(
-        self,
-        position: Position,
-        direction: Direction,
-        historical_walks: dict[
-            tuple[tuple[int, int], tuple[int, int]], list[tuple[np.matrix, int]]
-        ],
-    ) -> int | None:
-        # When not obstructed, to not use history
-        if not self.is_obstructed:
-            return False
-        # If we have never been here, we can't use history
-        if (position.tuple_, direction.tuple_) not in historical_walks:
-            return False
-        # we have been here
-        for mask, result in historical_walks[(position.tuple_, direction.tuple_)]:
-            # For this historical run, we have not been at the obstructed position
-            # hence, even with the obstruction, the result will be the same as previously
-            if mask[*self.obstructed_position.tuple_] != "X":
-                if self.obstructed_position.tuple_ in [
-                    (6, 3),
-                    (6, 7),
-                    (6, 8),
-                    (8, 1),
-                    (8, 3),
-                    (9, 7),
-                ]:
-                    test = ""
-                return result
-        return None
-
-    def walk_until_exit(
-        self,
-        position: Position = None,
-        direction: Direction = None,
-        guard_history: list[tuple[Position, Direction]] = None,
-    ) -> int | None:
-        position = position or Position(*np.argwhere(self.data == "^")[0])
-        direction = direction or Directions.TOP.value
-        self.mask = self.data.copy()
-        if self.is_obstructed:
-            self.mask[*self.obstructed_position.tuple_] = "O"
-        self.guard_history = guard_history or []
-        while self.is_in_bounds(position):
-            if (position, direction) in self.guard_history:
-                self.mask[*position.tuple_] = "!"
-                # print(self.obstructed_position)
-                # print(self.matrix_to_str(self.mask) + "\n\n")
-                return None
-            self.guard_history.append((position, direction))
-            self.mask[*position.tuple_] = self.DIRECTION_TO_CHAR[direction]
+    def walk_until_exit(self) -> dict[Position, list[Direction]]:
+        """
+        Walk the map until the guard would exit the map. Return the history of the guard's path
+        Raise InfiniteLoopError if the guard would loop
+        """
+        position = Position(*np.argwhere(self.data == "^")[0])
+        direction = Directions.TOP.value
+        guard_history = defaultdict(set)
+        # Until the guard would exit the map in the next step
+        while self.is_in_bounds(position + direction):
+            guard_history[position].add(direction)
             if self.is_blocked(position + direction):
                 direction = direction.turn_right()
+                continue
+            # Deja vu
+            if direction in guard_history[position + direction]:
+                raise InfiniteLoopError
             position += direction
-        return np.count_nonzero(np.isin(self.mask, ["^", ">", "v", "<", "!", "O"]))
 
-
-def try_one(map: Map, i: int) -> int:
-    position_to_block, _ = map.guard_history[i]
-    start_position, start_direction = map.guard_history[i - 1]
-    # start_direction = start_direction.turn_right()
-    map.obstruct(position_to_block)
-    result = map.walk_until_exit(
-        start_position, start_direction, [*map.guard_history[: i - 1]]
-    )
-    return 1 if result is None else 0
+        return guard_history
 
 
 class Puzzle6(PuzzleToSolve):
@@ -134,54 +72,26 @@ class Puzzle6(PuzzleToSolve):
         return 6
 
     def parse_input(self, input_: str) -> Map:
-        return Map(input_, pad=None, dtype=str)
+        return Map(input_)
 
-    def a(self, map: Map) -> int:
-        return map.walk_until_exit()
+    def a(self, map_: Map) -> int:
+        return len(map_.walk_until_exit())
 
-    def b(self, start_map: Map) -> int:
-        print("Starting b")
-        map_count = 0
-        original_map = deepcopy(start_map)
-        original_map.walk_until_exit()
-
-        # for i in tqdm(range(1, len(original_map.guard_history))):
-        # start = 4125 if len(original_map.guard_history) > 4120 else 1
-        start = 1
-        args = []
-        tried = []
-        for i in range(start, len(original_map.guard_history)):
-            position = original_map.guard_history[i][0]
-            if position in tried:
+    def b(self, map_: Map) -> int:
+        result = 0
+        guard_history = map_.walk_until_exit()
+        start = Position(*np.argwhere(map_.data == "^")[0])
+        for position in tqdm(guard_history.keys()):
+            if position == start:
                 continue
-            tried.append(position)
-            args.append((original_map, i))
-
-        result = pqdm(args, try_one, n_jobs=15, argument_type="args")
-
-        return sum(result)
-
-        for i in tqdm(range(start, len(original_map.guard_history))):
-            # for i in range(start, len(original_map.guard_history)):
-            # print(
-            #     f"{i}/{len(original_map.guard_history)} ({i/len(original_map.guard_history)*100:.2f}%)",
-            #     end="\r",
-            # )
-            position_to_block, _ = original_map.guard_history[i]
-            start_position, start_direction = original_map.guard_history[i - 1]
-            start_direction = start_direction.turn_right()
-            if start_position == Position(1, 4):
-                test = ""
-            # map = deepcopy(start_map)
-            start_map.obstruct(position_to_block)
-            result = start_map.walk_until_exit(
-                start_position, start_direction, [*original_map.guard_history[: i - 1]]
-            )
-            if result is None:
-                map_count += 1
-            start_map.destruct(position_to_block)
-
-        return map_count
+            map_[position] = "#"
+            try:
+                map_.walk_until_exit()
+            except InfiniteLoopError:
+                result += 1
+            finally:
+                map_[position] = "."
+        return result
 
 
 if __name__ == "__main__":
